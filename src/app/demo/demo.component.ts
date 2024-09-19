cec727964e72e09faa2c5422052676f7a798b242
martina <martinabo9@gmail.com>
2024-09-19 12:48:44 +0300

    lastupdate

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterLinkActive],
   templateUrl: './demo.component.html',
-  styleUrl: './demo.component.scss'
+  styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent {
 
